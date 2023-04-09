e32dfa33cdd625cb8e4568281c73dc3d36664dd2
Pedro Klein <79673266+pepeklein@users.noreply.github.com>
2023-04-09 20:57:21 -0300

    Add files via upload

---
+++ 5-Caracteres&Textos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a'; // importante manter aspas simples
+        Console.WriteLine(letra);
+
+        letra = (char)65; // ASCII table
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1); //ASCII table
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
